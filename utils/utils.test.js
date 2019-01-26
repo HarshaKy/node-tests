04efa15522cf12d867f824d05a31b174ecd064a7
HarshaKy <harshaky6573@gmail.com>
2019-01-26 16:31:19 +0530

    assertions

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,38 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square the input number', () => {
   var res = utils.square(3);
 
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}`);
-  }
+  expect(res).toBe(9).toBeA('number');
 });
+
+it('should set firstName and lastName', () => {
+  var user = {location: 'India', age: 21};
+  var res = utils.setName(user, 'Harsha Ky');
+
+  expect(res).toInclude({
+    firstName: 'Harsha',
+    lastName: 'Ky'
+  });
+});
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(123);
+//   // expect({name: 'Harsha'}).toEqual({name: 'Harsha'});
+//   // expect([2,3,4]).toInclude([44]);
+//   expect({
+//     name: 'Harsha',
+//     age: 21,
+//     location: 'India'
+//   }).toInclude({
+//     age: 21
+//   })
+// });
