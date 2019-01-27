df02b31eeff76e8879b008179a90c10c2c9e1c15
HarshaKy <harshaky6573@gmail.com>
2019-01-27 16:09:32 +0530

    describe method

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,41 +2,51 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11);
+describe('Utils', () => {
 
-  expect(res).toBe(44).toBeA('number');
-});
+  describe('Add and Square Sync', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(33, 11);
 
-it('should square the input number', () => {
-  var res = utils.square(3);
+      expect(res).toBe(44).toBeA('number');
+    });
 
-  expect(res).toBe(9).toBeA('number');
-});
+    it('should square the input number', () => {
+      var res = utils.square(3);
 
-it('should asyncAdd two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+      expect(res).toBe(9).toBeA('number');
+    });
+  });
+
+  describe('Add and Square Async', () => {
+    it('should asyncAdd two numbers', (done) => {
+      utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      });
+    });
+
+    it('should asyncSquare two numbers', (done) => {
+      utils.asyncSquare(8, (square) => {
+        expect(square).toBe(64);
+        done();
+      });
+    });
+  });
+
+  describe('Playground', () => {
+    it('should set firstName and lastName', () => {
+      var user = {location: 'India', age: 21};
+      var res = utils.setName(user, 'Harsha Ky');
+
+      expect(res).toInclude({
+        firstName: 'Harsha',
+        lastName: 'Ky'
+      });
+    });
   });
 });
 
-it('should asyncSquare two numbers', (done) => {
-  utils.asyncSquare(8, (square) => {
-    expect(square).toBe(64);
-    done();
-  });
-});
-
-it('should set firstName and lastName', () => {
-  var user = {location: 'India', age: 21};
-  var res = utils.setName(user, 'Harsha Ky');
-
-  expect(res).toInclude({
-    firstName: 'Harsha',
-    lastName: 'Ky'
-  });
-});
 
 // it('should expect some values', () => {
 //   // expect(12).toNotBe(123);
