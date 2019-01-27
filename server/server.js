7f92ba54b07a9d6ec0f99d47bef997421b1228ad
HarshaKy <harshaky6573@gmail.com>
2019-01-27 15:52:42 +0530

    supertest and expect

--- server/server.js
+++ server/server.js
@@ -3,7 +3,23 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.send([{
+    name: 'Mike',
+    age: 24
+  }, {
+    name: 'Harsha',
+    age: 21
+  }, {
+    name: 'Jeff',
+    age: 40
+  }])
 });
 
 app.listen(3000);
--- server/server.test.js
+++ server/server.test.js
@@ -1,3 +1,29 @@
 const request = require('supertest');
+const expect = require('expect');
 
 var app = require('./server').app;
+
+it('should return hello world', (done) => {
+  request(app)
+    .get('/')
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        error: 'Page not found.'
+      })
+    })
+    .end(done);
+});
+
+it('should return my user object', (done) => {
+  request(app)
+    .get('/users')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        name: 'Harsha',
+        age: 21
+      });
+    })
+    .end(done);
+});
